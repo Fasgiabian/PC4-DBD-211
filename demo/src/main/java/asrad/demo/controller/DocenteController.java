6e7b0224ec89bafd4d56e4a574ce27d19bfd9e60
GianlucaFasabi <gianlucaLICEO2000>
2021-07-15 20:50:43 -0500

    Tercer commit

--- demo/src/main/java/asrad/demo/controller/DocenteController.java
+++ demo/src/main/java/asrad/demo/controller/DocenteController.java
@@ -1,12 +1,16 @@
 package asrad.demo.controller;
 
 import asrad.demo.bean.Docente;
+import asrad.demo.bean.TrabajadorRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -30,5 +34,21 @@ public class DocenteController {
         st.close();
         con.close();
         return D;
+    };
+    @PostMapping("/TrabajadorNew")
+    public String newTrab(@RequestBody TrabajadorRequest tr) throws Exception{
+        Connection con = template.getDataSource().getConnection();
+        String sql = "INSERT INTO Trabajador VALUES (?,?,?,?,?,?);";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,tr.getCodtrabajador());
+        st.setString(2,tr.getNombre());
+        st.setString(3,tr.getAppat());
+        st.setString(4,tr.getApmat());
+        st.setString(5,tr.getContraseña());
+        st.setInt(6,tr.getCodrol());
+        int a = st.executeUpdate();
+        st.close();
+        con.close();
+        return String.valueOf(a);
     };
 }
